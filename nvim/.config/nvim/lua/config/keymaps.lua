1fc2958f624fc359ca32589ae9ba3e8769749ba5
¨Lali <micskeil@gmail.com>
2024-08-06 12:57:58 +0200

    update kanata alacritty lazy

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keYmaps here
 
 local map = LazyVim.safe_keymap_set
 -- save file
 vim.keymap.del("i", "<C-s>")
+
 map({ "i" }, "<C-s>", "<esc><esc>", { desc = "Save File" })
