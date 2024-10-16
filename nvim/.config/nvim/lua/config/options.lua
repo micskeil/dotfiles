820492b0f35f387c4f8da42a207982abdfd10bf3
¨Lali <micskeil@gmail.com>
2024-10-16 10:47:51 +0200

    update

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additional options hre
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
@@ -8,6 +8,9 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.background = "light"
+
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
 
 vim.opt.swapfile = false
 
