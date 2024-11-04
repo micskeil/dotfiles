123291d9734928eb8803fb0873889189696f09cc
¨Lali <micskeil@gmail.com>
2024-11-04 09:58:07 +0100

    update nvim + border dor the terminal inside neovim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,12 @@ local map = LazyVim.safe_keymap_set
 vim.keymap.del("i", "<C-s>")
 
 map({ "i" }, "<C-s>", "<esc><esc>", { desc = "Save File" })
+
+-- Add border to terminal
+local Util = require("lazyvim.util")
+vim.keymap.set("n", "<C-_>", function()
+  Util.terminal(nil, { border = "rounded" })
+end, { desc = "Term with border" })
+vim.keymap.set("n", "<C-/>", function()
+  Util.terminal(nil, { border = "rounded" })
+end, { desc = "Term with border" })
