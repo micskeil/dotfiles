90d24e76e7a2f027519407a67f6882f813fbfe4c
Lali Micskei <lali.micskei@gmail.com>
2025-04-04 15:09:01 +0200

    update nvim

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -2,7 +2,7 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = "*", -- set this if you want to always pull the latest change
+  version = false, -- set this if you want to always pull the latest change
   opts = {
     provider = "copilot",
     -- auto_suggestions_provider = "copilot",
